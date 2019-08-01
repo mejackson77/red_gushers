a7af42b7ba7c58beaddd74edbd7ef245a19c61d0
Marvin Jackson <marvinjackson@Marvins-Air.fios-router.home>
2019-07-31 23:19:20 -0400

    i just made an equal sign in between greater than equal operators

--- src/myFirstBot/birthYearCalculator.java
+++ src/myFirstBot/birthYearCalculator.java
@@ -13,25 +13,25 @@ public class birthYearCalculator {
 
 		int userEnteredNumber = birthYearVerification.nextInt();
 
-		if (userEnteredNumber > 90 && userEnteredNumber < 100) {
+		if (userEnteredNumber >= 90 && userEnteredNumber < 100) {
 			System.out.println("You were born in the 1920's. Yikes");
-		}else if (userEnteredNumber > 80 && userEnteredNumber < 90) {
+		}else if (userEnteredNumber >= 80 && userEnteredNumber < 90) {
 			System.out.println("You were born in the 1930's. Pretty old there bud");
-		}else if (userEnteredNumber > 70 && userEnteredNumber < 80) {
+		}else if (userEnteredNumber >= 70 && userEnteredNumber < 80) {
 			System.out.println("you were born in the 1940's. Are you rich yet?");
-		}else if (userEnteredNumber > 60 && userEnteredNumber < 70) {
+		}else if (userEnteredNumber >= 60 && userEnteredNumber < 70) {
 			System.out.println("you were born in the 1950's. Please tell me you're retired");
-		}else if (userEnteredNumber > 50 && userEnteredNumber < 60) {
+		}else if (userEnteredNumber >= 50 && userEnteredNumber < 60) {
 			System.out.println("you were born in the 1960's.");
-		}else if (userEnteredNumber > 40 && userEnteredNumber < 50) {
+		}else if (userEnteredNumber >= 40 && userEnteredNumber < 50) {
 			System.out.println("you were born in the 1970's. Ditch the fro bro");
-		}else if (userEnteredNumber > 30 && userEnteredNumber < 40) {
+		}else if (userEnteredNumber >= 30 && userEnteredNumber < 40) {
 			System.out.println("you were born in the 1980's. Blah");
-		}else if (userEnteredNumber >= 20 && userEnteredNumber < 30) {
+		}else if (userEnteredNumber >=20 && userEnteredNumber < 30) {
 			System.out.println("you were born in the 1990's. Lit Bro!!");
-		}else if (userEnteredNumber > 10 && userEnteredNumber < 20) {
+		}else if (userEnteredNumber >= 10 && userEnteredNumber < 20) {
 			System.out.println("you were born in the 2000's. Generation Z. Eww");
-		}else if (userEnteredNumber > 0 && userEnteredNumber < 10) {
+		}else if (userEnteredNumber >= 0 && userEnteredNumber < 10) {
 				System.out.println("you were born in the 2010's. Dont dissapoint us");
 		}else {
 			System.out.println("Yuu sure you're human!!!");
